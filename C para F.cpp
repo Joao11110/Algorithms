30570155b9bea2f17120fc12c6fab064844058a9
João Pedro <116277457+Joao11110@users.noreply.github.com>
2023-01-22 16:51:17 -0300

    Update C para F.cpp

--- C para F.cpp
+++ C para F.cpp
@@ -2,15 +2,11 @@
 #include<locale.h>
 
 int main(){
-	float f,c;
+    float f,c;
     setlocale(LC_ALL, "Portuguese");
     printf("Digite a temperatura em Celsius: ");
     scanf("%f",&c);
     f = (c*1.8) + 32;
-    printf("A temperatura em Fahrenheit é: %.1f",f);
+    printf("A temperatura em Fahrenheit Ã©: %.1f",f);
     return 0;
 }
-
-/*
-printf("Temperatura em Fahrenheit: %.1f",f=(c*1.8)+32);
-*/
